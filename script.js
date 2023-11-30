1dc17a722da2de0a34403858c3b7a31124e333f9
Alex <alexander.prohnitchi@gmail.com>
2023-11-30 20:06:19 +0100

    Prima versiune

---
+++ script.js
@@ -0,0 +1,40 @@
+
+for (let i = 0; i < array.length; i++) 
+{
+    const element = array[i];
+    
+}
+
+let text = 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Cupiditate, odit tempore dicta id voluptatum quisquam, quod, maiores numquam reiciendis dolorem eveniet iste! Porro laborum error voluptates ut totam quidem cupiditate.';
+
+
+// ==
+// ===
+
+let test = "1";
+{
+    let test = "2";
+
+
+}
+
+let arr = []
+
+function greetUser(user) {
+    console.log('Hello, ' + user);
+}
+
+let greetUser2 = (user) => console.log('Hello, ' + user)
+
+
+let numar = 5;
+
+for (let i = 0; i < 100; i++) {
+    
+}
+// 1) Repositoriu
+// 2) Commit
+// 3) Branch
+
+// 4) Merge
+// 5) Pushing
